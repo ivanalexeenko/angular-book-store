35b0656486c2da05185328864bab456120949bc5
DESKTOP-984MKD2\User <ivan.alexeenko@expert-soft.by>
2023-09-18 23:31:08 +0200

    Task 2: Buttons for Changing quantity and Removing cart items added

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItemModel } from '../cartItemModel';
 
 @Component({
@@ -8,4 +8,20 @@ import { CartItemModel } from '../cartItemModel';
 })
 export class CartItemComponent {
   @Input() cartItem = {} as CartItemModel;
+
+  @Output() decreaseQuantityEvent = new EventEmitter<CartItemModel>;
+  @Output() increaseQuantityEvent = new EventEmitter<CartItemModel>;
+  @Output() removeEvent = new EventEmitter<CartItemModel>;
+
+  onQuantityDecrease(cartItem: CartItemModel) {
+    this.decreaseQuantityEvent.emit(cartItem);
+  }
+
+  onQuantityIncrease(cartItem: CartItemModel) {
+    this.increaseQuantityEvent.emit(cartItem);
+  }
+
+  onRemove(cartItem: CartItemModel) {
+    this.removeEvent.emit(cartItem);
+  }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CartModel } from '../cartModel';
+import { CartItemModel } from '../cartItemModel';
 
 @Component({
   selector: 'app-cart',
@@ -8,4 +9,28 @@ import { CartModel } from '../cartModel';
 })
 export class CartComponent {
   @Input() cart = {} as CartModel;
+
+  increaseItemQuantity(cartItem: CartItemModel) {
+    if (this.cart.cartItems.includes(cartItem)) {
+      cartItem.quantity++;
+    }
+  }
+
+  decreaseItemQuantity(cartItem: CartItemModel) {
+    if (this.cart.cartItems.includes(cartItem)) {
+      if (cartItem.quantity > 1) {
+        cartItem.quantity--;
+      }
+    }
+  }
+
+  removeItem(cartItem: CartItemModel) {
+    if (this.cart.cartItems.includes(cartItem)) {
+      let indexToRemove = this.cart.cartItems.indexOf(cartItem);
+
+      if (indexToRemove !== -1) {
+        this.cart.cartItems.splice(indexToRemove, 1);
+      }
+    }
+  }
 }
